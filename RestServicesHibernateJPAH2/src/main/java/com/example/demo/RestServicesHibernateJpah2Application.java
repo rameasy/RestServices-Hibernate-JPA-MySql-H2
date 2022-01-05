e699f1918275b0c70175aaa80ff3f54d17648397
Ramachandran Subramanian <rameasy@gmail.com>
2022-01-05 20:51:30 +0000

    Created new project to test Rest API with Hibernate, JPA and H2 Database

---
+++ RestServicesHibernateJPAH2/src/main/java/com/example/demo/RestServicesHibernateJpah2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesHibernateJpah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesHibernateJpah2Application.class, args);
+	}
+
+}
